50fab38c7f75339c4ce7213b67014100f835a25a
Giovanni Volpintesta <giovanni.volpintesta@gmail.com>
2017-01-22 16:38:59 +0100

    Credits

---
+++ Assets/Scripts/CreditSceneManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditSceneManager : MonoBehaviour {
+
+    public string startGameScene;
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(startGameScene);
+    }
+
+}
